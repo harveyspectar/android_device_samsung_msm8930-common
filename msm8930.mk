e77e2bb9e735897c1e58dd44070dfedab6098f46
harveyspectar <ultimatepravin72@gmail.com>
2017-08-01 09:17:28 +0200

    msm8930: fix conflicting hals

--- msm8930.mk
+++ msm8930.mk
@@ -46,8 +46,7 @@ PRODUCT_PACKAGES += \
     copybit.msm8960 \
     gralloc.msm8960 \
     hwcomposer.msm8960 \
-    memtrack.msm8960 \
-    power.msm8960
+    memtrack.msm8960
 
 # Media
 PRODUCT_PACKAGES += \
